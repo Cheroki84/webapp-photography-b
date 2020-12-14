bf7b373972fe94c4e439168b59190a14182bf133
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-12-14 17:46:26 +0100

    added endpoint update for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -135,7 +135,44 @@ const UserController = {
                 error
             })
         }
-    }
+    },
+
+    async update(req, res) {
+        let regExPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+        let regExEmail = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/;
+
+        if (!regExEmail.test(req.body.email)) {
+            res.send({
+                message: 'El formato del email no es válido'
+            });
+            return;
+        }
+
+        if (!regExPassword.test(req.body.password)) {
+            res.send({
+                message: 'El password debe contener al menos: entre 8 y 16 caracteres, 1 número, 1 letra minúscula, 1 letra mayúscula y 1 carácter especial'
+            });
+            return;
+        }
+
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 10)
+            const userUpdate = await User.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            });
+            res.status(201).send({
+                message: 'Los datos se han actualizado correctamente',
+                userUpdate
+            })
+        } catch (error) {
+            res.status(500).send({
+                message: 'Hubo un error al intentar actualizar los datos',
+                error
+            });
+        }
+    },
 }
 
 module.exports = UserController;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,5 +6,6 @@ router.post('/login', UserController.login);
 router.put('/logout/:email', UserController.logout);
 router.delete('/delete', UserController.delete);
 router.get('/allUsers', UserController.getAll);
+router.put('/update/:id', UserController.update);
 
 module.exports = router;
