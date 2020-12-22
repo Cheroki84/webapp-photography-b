c1964b8c2fd9956e982edc05392e36b3f8441d37
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-12-22 16:57:24 +0100

    added middleware for admin role

--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const AppointmentController = require('../controllers/appointmentController');
 const auth = require('../middleware/auth');
+const checkRole = require('../middleware/checkRole');
 
 router.post('/create', auth, AppointmentController.create);
 router.put('/update/:id', auth, AppointmentController.update);
 router.delete('/delete/:id', auth, AppointmentController.delete);
 router.delete('/deleteByDateAppointmentId/:DateappointmentId', auth, AppointmentController.deleteByDateAppointmentId);
 router.get('/allAppointments', auth, AppointmentController.getAll);
-router.get('/allWithUsers', auth, AppointmentController.getAllWithUsers);
+router.get('/allWithUsers', auth, checkRole(['admin']), AppointmentController.getAllWithUsers);
 router.get('/byUserId/:UserId', auth, AppointmentController.getByUserId);
 
 module.exports = router;
